cb99aeef638b3552e550206cd33529793c8a3661
Ygor raphael <ygoraphael@gmail.com>
2022-07-30 02:35:59 +0100

    Criado todo-list com comandos basicos utilizando apenas html, css e javascript

---
+++ script.js
@@ -0,0 +1,29 @@
+window.onload = (event) => {
+    AllEvents();
+};
+function AllEvents(){    
+    document.getElementById("button").addEventListener('click', el=>{  
+        el.preventDefault();
+        const Input = document.getElementById("todoInput");
+        
+        let itemId = String(Date.now());
+        addItem(itemId , Input.value);
+        document.getElementById("todoInput").value = ''
+    });
+
+    function addItem(itemId, Item) {    
+        const ul = document.getElementById('toDoList');
+        const li = document.createElement('li')
+        li.setAttribute("data-id", itemId);
+        li.innerText = Item;
+        ul.appendChild(li);
+    }
+
+    document.getElementById("toDoList").addEventListener('click', el=>{
+        let id = el.target.getAttribute('data-id')
+        if (!id) return
+        const ul = document.getElementById('toDoList'); 
+        var li = document.querySelector('[data-id="' + id + '"]');
+        ul.removeChild(li);
+    }); 
+};
